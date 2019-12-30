35ec8f0f7fca4a9cec74e858d0a57ff7444ecbf9
wutt <1160233058@qq.com>
2019-12-30 17:58:05 +0800

    项目构建

--- leyou-springcloud/leyou/leyou-registry/src/main/java/com/leyou/RegistryApplication.java
+++ leyou-springcloud/leyou/leyou-registry/src/main/java/com/leyou/RegistryApplication.java
@@ -1,10 +1,19 @@
 package com.leyou;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 /**
  * Author: 吴涛涛
  * Date : 2019 - 12 - 30 11:32
  * Description:<描述>
  * Version: 1.0
  */
+@SpringBootApplication
+@EnableEurekaServer
 public class RegistryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RegistryApplication.class,args);
+    }
 }
